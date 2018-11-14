250a6efbf11bceea7e825152f611a2c44122ea84
Gabriela Martinez <gmartinezcampos@mac.com>
2018-11-14 15:06:14 +0000

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create!(name: 'Gaby', email: 'gaby@gmail.com', password: '123456')
+User.create!(name: 'izzy', email: 'izzy@gmail.com', password: '123456')
+User.create!(name: 'lucien', email: 'lucien@gmail.com', password: '123456')
+User.create!(name: 'ife', email: 'ife@gmail.com', password: '123456')
+
+10.times do
+  Boat.create!(
+    name: Faker::Pokemon.name,
+    description: Faker::Lorem.paragraph,
+    location: Faker::Address.street_address,
+    capacity: rand(0..10),
+    length: rand(0..1000),
+    price_per_week: rand(0..1000),
+    category: ['Sail Boat', 'Motor Boat'].sample,
+    user: User.all.sample
+  )
+end
+
+10.times do
+  Review.create!(
+    content: Faker::Lorem.paragraph,
+    rating: rand(1..5),
+    boat: Boat.all.sample,
+    user: User.all.sample
+    )
+end
+
+10.times do
+  Booking.create!(
+    start_date: Faker::Date.between_except(1.year.ago, 1.year.from_now, Date.today),
+    end_date: Faker::Date.between_except(1.year.ago, 1.year.from_now, Date.today),
+    boat: Boat.all.sample,
+    user: User.all.sample
+    )
+end
+
+
+
