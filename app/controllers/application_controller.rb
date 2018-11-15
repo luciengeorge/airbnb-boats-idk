e67c10f206d74898f2a5a677f15ee2c9a5cdd00a
Gabriela Martinez <gmartinezcampos@mac.com>
2018-11-15 15:18:51 +0000

    profile page edits, with avatar uploader and logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
   end
 end
