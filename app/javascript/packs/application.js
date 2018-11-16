54bd962556a48a8c186ef59c141908aaaddb57a6
Ife Odugbesan <ifeodugbesan@yahoo.com>
2018-11-16 08:22:51 +0000

    fixed margin div and location search on home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,12 @@
 import "bootstrap";
 import './components/modal';
 import './components/map'
+import './components/flash';
 
 
 // import { fixReviewScroll } from './components/reviews';
 // fixReviewScroll()
 
 // import { fixReviewScroll } from './components/reviews';
-// import './components/flash';
 // fixReviewScroll()
 
